df12879ef14936c8b2c413347f6fd2737f0d04c7
chahlaouy <chahlaouy1991@gmail.com>
2021-05-23 22:43:19 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,13 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MenuComponent } from './layouts/menu/menu.component'
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent, 
+    MenuComponent
+  ],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
